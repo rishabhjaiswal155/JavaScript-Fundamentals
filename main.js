3454c7be813455cc6bd76040316e999f5471036f
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-07-10 17:51:53 +0530

    Equality in Javascript(== only value check === value+type check i.e strict checking

--- main.js
+++ main.js
@@ -112,52 +112,77 @@
 
 //Implicit type conversion(type coercion)
 
-console.log('5'+'4')
-console.log('4'+true)
-console.log('5'+undefined)
-console.log('5'+'Rishabh')
-console.log(true+'4')
-console.log('5'-'4')
-console.log('4'-true)
-console.log('4'-false)
-console.log('Rishabh'-'Jaiswal')
-console.log('10'-null)
-console.log('10'-NaN)
-console.log('10'-undefined)
-console.log(true+true)
-console.log(false+false)
-console.log(true+null)
-console.log(true+NaN)
-console.log(true+undefined)
+// console.log('5'+'4')
+// console.log('4'+true)
+// console.log('5'+undefined)
+// console.log('5'+'Rishabh')
+// console.log(true+'4')
+// console.log('5'-'4')
+// console.log('4'-true)
+// console.log('4'-false)
+// console.log('Rishabh'-'Jaiswal')
+// console.log('10'-null)
+// console.log('10'-NaN)
+// console.log('10'-undefined)
+// console.log(true+true)
+// console.log(false+false)
+// console.log(true+null)
+// console.log(true+NaN)
+// console.log(true+undefined)
 
 
-//Explicit type conversion
+// //Explicit type conversion
 
-console.log(Number('5'))
-console.log(Number(true))
-console.log(Number(false))
-console.log(Number(''))
-console.log(Number(null))
-console.log(Number(undefined))
+// console.log(Number('5'))
+// console.log(Number(true))
+// console.log(Number(false))
+// console.log(Number(''))
+// console.log(Number(null))
+// console.log(Number(undefined))
 
-console.log(String(2))
-console.log(String(true))
-console.log(String(null))
-console.log(String(undefined))
+// console.log(String(2))
+// console.log(String(true))
+// console.log(String(null))
+// console.log(String(undefined))
 
-console.log(parseInt('3'))
-console.log(parseFloat('3.14'))
-console.log(parseInt(true))
+// console.log(parseInt('3'))
+// console.log(parseFloat('3.14'))
+// console.log(parseInt(true))
 
-console.log((500).toString())
-console.log((true).toString())
+// console.log((500).toString())
+// console.log((true).toString())
 
-console.log(Boolean(10))
-console.log(Boolean(null))
-console.log(Boolean(''))
-console.log(Boolean(undefined))
-console.log(Boolean(NaN))
-console.log(Boolean(0))
+// console.log(Boolean(10))
+// console.log(Boolean(null))
+// console.log(Boolean(''))
+// console.log(Boolean(undefined))
+// console.log(Boolean(NaN))
+// console.log(Boolean(0))
 
 
+//Equality
 
+const var1='test'
+const var2='test'
+console.log(var1==var2)
+console.log(var1===var2)
+
+const a=10
+const b='10'
+console.log(a==b)
+console.log(a===b)
+
+const x=true
+const y=1
+console.log(x==y)
+console.log(x===y)
+
+const p=false
+const q=''
+console.log(p==q)
+console.log(p===q)
+
+const m=null
+const n=undefined
+console.log(m==n)
+console.log(m===n)
