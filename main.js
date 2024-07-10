9f653322f2a528217df530c610ccb550dd30a5dd
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-07-10 16:29:05 +0530

    Operators in Javascript

--- main.js
+++ main.js
@@ -13,49 +13,97 @@
 //Data-types(Primitive and non-primitive)
 
 //String data-type
-const name = "Rishabh";
-const language = "Javascript";
-const institute = `IACSD`;
+// const name = "Rishabh";
+// const language = "Javascript";
+// const institute = `IACSD`;
 
-console.log(name);
-console.log(language);
-console.log(institute);
+// console.log(name);
+// console.log(language);
+// console.log(institute);
 
-//number data-type
+// //number data-type
 
-let num = 10;
-num = 3.14;
-console.log(num);
+// let num = 10;
+// num = 3.14;
+// console.log(num);
 
-//Boolean data-type
+// //Boolean data-type
 
-const isPrimaryNumber = "true";
-const isOddNumber = "false";
-console.log(isPrimaryNumber);
-console.log(isOddNumber);
+// const isPrimaryNumber = "true";
+// const isOddNumber = "false";
+// console.log(isPrimaryNumber);
+// console.log(isOddNumber);
 
-//undefined data-type
+// //undefined data-type
 
-let a;
-console.log(a);
+// let a;
+// console.log(a);
 
-//Null Data-type
+// //Null Data-type
 
-const d = null;
-console.log(d);
+// const d = null;
+// console.log(d);
 
-//nonPrimitive Object data-type
+// //nonPrimitive Object data-type
 
-const person = {
-  firstName: "Rishabh",
-  lastName: "Jaiswal",
-  age: 28,
-  "email-id": "rishabhjaiswal155@gmail.com",
-};
-console.log(person.firstName)
+// const person = {
+//   firstName: "Rishabh",
+//   lastName: "Jaiswal",
+//   age: 28,
+//   "email-id": "rishabhjaiswal155@gmail.com",
+// };
+// console.log(person.firstName)
 
 
-//Array data-type
+// //Array data-type
 
-const oddNumber=[1,3,5,7,9]
-console.log(oddNumber[2])
+// const oddNumber=[1,3,5,7,9]
+// console.log(oddNumber[2])
+
+
+//operators
+
+//Assignment Operator
+
+let a=20
+let b=10
+
+//Arithmetic operator
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(++a)
+console.log(--b)
+
+
+//Comparision operator
+
+console.log(a>b)
+console.log(a<b)
+console.log(a==b)
+console.log(a===b)
+console.log(a>=b)
+console.log(a<=b)
+console.log(a!==b)
+console.log(a!=b)
+
+
+//Logical Operator
+
+const isValid=a>b || b<20
+console.log(isValid)
+
+const isReady=true
+console.log(!isReady)
+
+//String Operator
+ 
+console.log('Rishabh '+'Jaiswal')
+
+//Ternary operator
+
+const isEven=11%2===0 ? 'number is Even':'number is odd'
+console.log(isEven)
+
