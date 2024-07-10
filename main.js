8e0b0552724797a530e2983ac4d4a77a5a3555d4
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-07-10 15:59:44 +0530

    Data-types (Primitive and Non-primitive) in Javascript

--- main.js
+++ main.js
@@ -1,12 +1,61 @@
-console.log('Hello World from main.js')
+// console.log('Hello World from main.js')
 
-//variables
-//let age --error free 
-let age=28
-age=30// no error
-console.log(age)
-//const salary---error
-const salary=50000
-//salary=80000//error(not allowed re-assignment)
-console.log(salary)
+// //variables
+// //let age --error free
+// let age=28
+// age=30// no error
+// console.log(age)
+// //const salary---error
+// const salary=50000
+// //salary=80000//error(not allowed re-assignment)
+// console.log(salary)
 
+//Data-types(Primitive and non-primitive)
+
+//String data-type
+const name = "Rishabh";
+const language = "Javascript";
+const institute = `IACSD`;
+
+console.log(name);
+console.log(language);
+console.log(institute);
+
+//number data-type
+
+let num = 10;
+num = 3.14;
+console.log(num);
+
+//Boolean data-type
+
+const isPrimaryNumber = "true";
+const isOddNumber = "false";
+console.log(isPrimaryNumber);
+console.log(isOddNumber);
+
+//undefined data-type
+
+let a;
+console.log(a);
+
+//Null Data-type
+
+const d = null;
+console.log(d);
+
+//nonPrimitive Object data-type
+
+const person = {
+  firstName: "Rishabh",
+  lastName: "Jaiswal",
+  age: 28,
+  "email-id": "rishabhjaiswal155@gmail.com",
+};
+console.log(person.firstName)
+
+
+//Array data-type
+
+const oddNumber=[1,3,5,7,9]
+console.log(oddNumber[2])
