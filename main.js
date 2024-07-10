f178bd638155f1b29399ab7b560b8a2e94385c45
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-07-10 15:34:35 +0530

    Variables in JavaScript

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('Hello World from main.js')
+
+//variables
+//let age --error free 
+let age=28
+age=30// no error
+console.log(age)
+//const salary---error
+const salary=50000
+//salary=80000//error(not allowed re-assignment)
+console.log(salary)
+
